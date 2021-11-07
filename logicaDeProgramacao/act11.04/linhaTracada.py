f0f1e02debcfb2fa0c277cc7a81c748d858d6dcb
Sabrina de Sousa <sabrinapaulla1@gmail.com>
2021-11-07 20:07:07 -0300

    Usando a biblioteca Graphics do Python

--- logicaDeProgramacao/act11.04/linhaTracada.py
+++ logicaDeProgramacao/act11.04/linhaTracada.py
@@ -4,31 +4,6 @@ a principal e a secundária na extensão máxima visível da janela em questão.
 (Enfim, é o mesmo de fazer um X na janela).
 A janela deve ser “quadrada”, Utilize: 600 x 600 pixels(colunas x linhas).
 '''
-import time
-
-
-from graphics import *
-import time
-
-janela = GraphWin("Fazer um X na tela ...", 600, 600)
-# Diagonal principal
-for linhas in range(600):
-   ponto = Line(Point(0, 600), Point(600, 0))
-   ponto.setFill("black")
-   ponto.draw(janela)
-   time.sleep(0.01)
-
-# Diagonal secundaria
-for colunas in range(600):
-   ponto = Point(colunas, colunas)
-   ponto.setFill("red")
-   ponto.draw(janela)
-   time.sleep(0.01)
-
-# Aguardando um clique de mouse para fechar a janela
-janela.getMouse()
-janela.close()
-
 
 from graphics import *
 
